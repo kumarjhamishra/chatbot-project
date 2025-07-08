b165d1c7cc7e6ed0103ac275339338fa0c8be4b0
kumarjhamishra <03satyam05@gmail.com>
2025-07-09 01:41:10 +0530

    Apply proper .gitignore and cleanup

--- backend/dist/config/openai-config.js
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-export const configureOpenAI = () => {
-    return new OpenAI({
-        apiKey: process.env.OPEN_AI_FREE_SECRET,
-        organization: process.env.OPENAI_ORGANIZATION_ID,
-    });
-};
-//# sourceMappingURL=openai-config.js.map
--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRoutes from "./user-route.js";
-import chatRoutes from "./chat-routes.js";
-const router = Router();
-router.use("/user", userRoutes);
-router.use("/chat", chatRoutes);
-export default router;
-//# sourceMappingURL=index.js.map
