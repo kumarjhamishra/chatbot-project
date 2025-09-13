3d6cba08b1a28bea923d3408971c7ca76b86d106
kumarjhamishra <03satyam05@gmail.com>
2025-09-13 12:41:41 +0530

    changes url to deployed backend url in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,12 +10,14 @@ import { Toaster } from "react-hot-toast";
 
 import axios from "axios";
 // set the base url
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+// axios.defaults.baseURL = "http://localhost:5000/api/v1";
 
 // changing connection to deployed backend
 //axios.defaults.baseURL = "https://chatbot-project-backend.onrender.com/api/v1";
 
 // allowing transfer of cookies from frontend to backend
+
+axios.defaults.baseURL = "https://chatbot-project-backend.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 // get the theme for the material ui
